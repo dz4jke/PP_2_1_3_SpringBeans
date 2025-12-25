3cc16260470f0a16543bb27c64ceca6013498da2
Denis <dz4jke@gmail.com>
2025-12-25 10:58:54 +0300

    newCommit

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,21 @@
 package app.config;
 
+import app.model.Cat;
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "cat")
+    public Cat cat() {
+        return new Cat();
+    }
+
+    @Bean(name = "dog")
+    public Dog dog() {
+        return new Dog();
+    }
 }
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,8 @@
+package app.model;
+
+public class Dog extends Animal {
+    @Override
+    public String toString() {
+        return "I am a Dog";
+    }
+}
